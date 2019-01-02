b86f675dbe87b0c16a08f26947de9b72ed1a459e
Fadlika Dita Nurjanto <fadlikadn@gmail.com>
2019-01-02 10:20:58 +0700

    Add firebase config file and components to add, edit and delete

---
+++ src/Firebase.js
@@ -0,0 +1,27 @@
+import * as firebase from 'firebase';
+import firestore from 'firebase/firestore';
+
+const settings = {timestampsInSnapshots: true};
+
+// const config = {
+//     apiKey: "YOUR_API_KEY",
+//     authDomain: "YOUR_AUTH_DOMAIN",
+//     databaseURL: "YOUR_DATABASE_URL",
+//     projectId: "YOUR_PROJECT_ID",
+//     storageBucket: "YOUR_STORAGE_BUCKET",
+//     messagingSenderId: "YOUR_MESSAGING_ID"
+// };
+
+const config = {
+    apiKey: "AIzaSyBNChqhGgrCig5CE3cmIDD9lcSzxvxu4zk",
+    authDomain: "react-firestore-beeb5.firebaseapp.com",
+    databaseURL: "https://react-firestore-beeb5.firebaseio.com",
+    projectId: "react-firestore-beeb5",
+    storageBucket: "react-firestore-beeb5.appspot.com",
+    messagingSenderId: "507097163162"
+};
+firebase.initializeApp(config);
+
+firebase.firestore().settings(settings);
+
+export default firebase;
