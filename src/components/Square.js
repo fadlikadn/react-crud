5327c32819e07bdfb1b7c2e50e4609803ca13281
Fadlika Dita Nurjanto <fadlikadn@gmail.com>
2019-01-03 13:48:45 +0700

    Make tic tac toe game working

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
 class Square extends Component {
+    
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         value: null,
+    //     };
+    // }
+    
     render() {
         return (
-            <button className="square">
-                {/* TODO */}
+            <button className="square" 
+                    // onClick={() => this.setState({value: 'X'}) }>
+                    onClick={() => this.props.onClick() }>
+                {this.props.value}
+                {/* {this.state.value} */}
             </button>
         );
     }
